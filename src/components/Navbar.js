3490c9ca66ba169cbb0dd4998355e3279453f1eb
Oinari-Shinto <oinari.shinto@gmail.com>
2021-09-17 23:54:05 +0300

    update text menu bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,7 +14,7 @@ const NavBar = () => {
             </div>
             <div className="pr-8 md:block hidden">
                 <Link className="p-4" to='/'>Главная</Link>
-                <Link className="p-4" to='/menu'>Menu</Link>
+                <Link className="p-4" to='/menu'>Камеры</Link>
                 <Link className="p-4" to='/about'>О нас</Link>
                 <Link className="p-4" to='/contact'>Контакты</Link>
             </div>
