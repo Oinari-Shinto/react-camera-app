b3c0fc304a06cca1cc48b4ad1a5aa8f17a1d44e7
Oinari-Shinto <oinari.shinto@gmail.com>
2021-10-13 18:37:07 +0300

    update content page

--- src/components/Content.js
+++ src/components/Content.js
@@ -7,7 +7,7 @@ const Content = () => {
         <div className="flex flex-col justify-center items-center bg-white h-screen font-mono py-40">
             <img src={CameraTwo} alt="" className="h-full rounded mb-20 " />
             <div className="flex flex-col justify-center items-center">
-            <h2 className="tent-2xl mb-2">Выбор камер и Гарантия Производителя</h2>
+            <h3 className="tent-2xl mb-2">Выбор камер и Гарантия Производителя</h3>
             <p className='mb-2' >Надежно, качественно с гарантией!</p>
             <span> </span>
             {/* <img src={CameraTwo} alt="" className="h-full rounded mb-20 shadow m-5" /> */}
