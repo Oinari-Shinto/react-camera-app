846d4b97d3a51d0c066159af876ef57d69bbabf6
Oinari-Shinto <oinari.shinto@gmail.com>
2022-02-09 23:55:43 +0300

    change text

--- src/components/Content.js
+++ src/components/Content.js
@@ -9,6 +9,7 @@ const Content = () => {
             <div className="flex flex-col justify-center items-center">
             <h3 className="tent-2xl mb-2">Выбор камер и Гарантия Производителя</h3>
             <p className='mb-2' >Надежно, качественно с гарантией!</p>
+            <p className='mb-2' >Надежно, качественно с гарантией!</p>
             <span> </span>
             {/* <img src={CameraTwo} alt="" className="h-full rounded mb-20 shadow m-5" /> */}
             
